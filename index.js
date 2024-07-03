316c5520a5acb070b00558a5e3828ceeb034604d
Panji97 <panjisetyokurniawan@gmail.com>
2024-07-03 17:56:41 +0700

    update response ddata

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 Pilih Akun: Mitra BPJS Kesehatan
 Username: ciciliachenny
 Password: @CiciliaChenny123
- */
+*/
 
 import crypto from 'crypto'
 import LZString from 'lz-string'
@@ -16,6 +16,7 @@ const userKey = 'c13c4c69d6650de32502418a28d6c382'
 const username = 'dvlp-cicilia'
 const password = 'Cicilia2024@'
 const kdAplikasi = '095'
+const key = `${consumerID}${consumerSecret}${timestamp}`
 
 const generateSignature = (value, key) => {
   return crypto.createHmac('sha256', key).update(value).digest('base64')
@@ -29,41 +30,40 @@ const headers = {
   user_key: userKey,
 }
 
-const params1 = '0000039043765'
+function decrypt(key, txt_enc) {
+  // Buat hash SHA-256 dari kunci
+  const keyHash = crypto.createHash('sha256').update(key, 'utf-8').digest()
+
+  // Konfigurasi mode AES dengan CBC dan IV dari key hash
+  const iv = keyHash.slice(0, 16)
+  const decipher = crypto.createDecipheriv('aes-256-cbc', keyHash.slice(0, 32), iv)
+
+  // Dekripsi teks terenkripsi dan ubah ke string
+  let decrypted = decipher.update(txt_enc, 'base64', 'utf-8')
+  decrypted += decipher.final('utf-8')
+
+  // Dekompresi teks yang telah didekripsi
+  const decompressed = LZString.decompressFromEncodedURIComponent(decrypted)
+
+  return decompressed
+}
+
+const params1 = '0000201419177'
 const params2 = '2'
 const params3 = '1'
 
-fetch(`https://apijkn-dev.bpjs-kesehatan.go.id/pcare-rest-dev/spesialis`, {
-  method: 'GET',
-  headers: headers,
-})
-  .then((response) => response.json())
-  .then((data) => {
-    function decrypt(key, txt_enc) {
-      // Buat hash SHA-256 dari kunci
-      const keyHash = crypto.createHash('sha256').update(key, 'utf-8').digest()
-
-      // Konfigurasi mode AES dengan CBC dan IV dari key hash
-      const iv = keyHash.slice(0, 16)
-      const decipher = crypto.createDecipheriv('aes-256-cbc', keyHash.slice(0, 32), iv)
-
-      // Dekripsi teks terenkripsi dan ubah ke string
-      let decrypted = decipher.update(txt_enc, 'base64', 'utf-8')
-      decrypted += decipher.final('utf-8')
-
-      // Dekompresi teks yang telah didekripsi
-      const decompressed = LZString.decompressFromEncodedURIComponent(decrypted)
-
-      return decompressed
-    }
-
-    const key = `${consumerID}${consumerSecret}${timestamp}`
-
-    if (data.response) {
-      const result = decrypt(key, data.response)
-      console.log(result)
-    } else {
-      console.error('Data response is null')
-    }
+const fetchDataJKN = async () => {
+  const response = await fetch(`https://apijkn-dev.bpjs-kesehatan.go.id/pcare-rest-dev/peserta/${params1}`, {
+    method: 'GET',
+    headers: headers,
   })
-  .catch((error) => console.error(error))
+
+  const repsonseJson = await response.json()
+
+  const dataResponse = decrypt(key, repsonseJson.response)
+  repsonseJson['response'] = JSON.parse(dataResponse)
+
+  console.log(repsonseJson)
+}
+
+fetchDataJKN()
